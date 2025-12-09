dbc0424a35a668575fdf208d6c1cafe944e1de5a
Dmitriy Zaruba <83030369+sergeevgit1@users.noreply.github.com>
2025-12-09 04:43:11 +0300

    Refactor configs into turborepo packages

---
+++ packages/config/eslint.config.js
@@ -0,0 +1,13 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const path = require('path');
+
+const compat = new FlatCompat({
+  baseDirectory: path.join(__dirname, '..', '..', 'apps', 'frontend'),
+});
+
+module.exports = [
+  {
+    ignores: ['**/node_modules/**', '**/.next/**', 'apps/backend/**'],
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+];
